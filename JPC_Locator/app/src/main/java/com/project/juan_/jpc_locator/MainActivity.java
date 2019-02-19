e98266358c0ddb617453aaf389418e687798ef37
juanpabloclaros <juan_pablo_claros@hotmail.com>
2019-02-19 20:11:47 +0000

    Added sign out functionality

--- JPC_Locator/app/src/main/java/com/project/juan_/jpc_locator/MainActivity.java
+++ JPC_Locator/app/src/main/java/com/project/juan_/jpc_locator/MainActivity.java
@@ -2,12 +2,31 @@ package com.project.juan_.jpc_locator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private Button btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnSignOut = (Button) findViewById(R.id.signOut);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
     }
 }
